b5dcdb70a5987d63349ced33e36876741e87767d
jmei403 <jason.mei403@gmail.com>
2025-01-08 14:19:26 -0500

    create product page and zustand setup

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import { useState } from 'react';
+import { Container, Heading, VStack, Box, useColorModeValue, Input, Button, useToast } from '@chakra-ui/react';
+import { useProductStore } from '../store/product';
 
 const CreatePage = () => {
-  return <div>CreatePage</div>;
+  const [newProduct, setNewProduct] = useState({
+    name: '',
+    price: '',
+    image: '',
+  });
+  const { createProduct } = useProductStore();
+  const toast = useToast();
+
+  const handleAddProduct = async () => {
+    const { success, message } = await createProduct(newProduct);
+
+    if (!success) {
+      toast({
+        title: 'Error',
+        description: message,
+        status: 'error',
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: 'Success',
+        description: message,
+        status: 'success',
+        isClosable: true,
+      });
+    }
+
+    setNewProduct({ name: '', price: '', image: '' });
+  };
+
+  return (
+    <Container>
+      <VStack spacing={8}>
+        <Heading as={'h1'} size={'2x1'} textAlign={'center'} mb={8}>
+          Create New Product
+        </Heading>
+        <Box w={'full'} bg={useColorModeValue('white', 'gray.800')} p={6} rounded={'lg'} shadow={'md'}>
+          <VStack spacing={4}>
+            <Input placeholder="Product Name" name="name" value={newProduct.name} onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })} />
+            <Input placeholder="Product Price" name="price" value={newProduct.price} onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })} />
+            <Input placeholder="Product Image" name="image" value={newProduct.image} onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })} />
+            <Button colorScheme="blue" onClick={handleAddProduct} w={'full'}>
+              Add Product
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
+  );
 };
 
 export default CreatePage;
