c8cf9d5e5ec45907b46a378bd5bfde9b1574f1cf
jmei403 <jason.mei403@gmail.com>
2025-01-08 15:44:23 -0500

    product card component & delete product functionality

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -36,7 +36,7 @@ const CreatePage = () => {
   return (
     <Container>
       <VStack spacing={8}>
-        <Heading as={'h1'} size={'2x1'} textAlign={'center'} mb={8}>
+        <Heading as={'h1'} size={'2xl'} textAlign={'center'} mb={8}>
           Create New Product
         </Heading>
         <Box w={'full'} bg={useColorModeValue('white', 'gray.800')} p={6} rounded={'lg'} shadow={'md'}>
